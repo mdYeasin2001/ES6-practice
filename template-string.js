89c46be1e16a9afcc0a899a8e4e4eae77e565859
Yeasin <yeasin30112001@gmail.com>
2021-02-02 20:20:31 -0600

    string template with decktext

---
+++ template-string.js
@@ -0,0 +1,15 @@
+const firstName = "Md";
+const lastName = "Yeasin";
+const fullName = firstName + " " + lastName + " is a good boy";
+const fullName2 = `${firstName} ${lastName} is a good boy`;
+// console.log(fullName2);
+
+
+const text = 'I love you \n'
++'I miss you\n'
++ 'I need you';
+
+const text2 = `I love you
+I miss you
+I need you`;
+console.log(text2);
